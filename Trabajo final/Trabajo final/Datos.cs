5805336b329e4b1bd19cf661b0dc813fa2bee8ee
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-08-04 14:32:46 -0400

    Add files via upload

---
+++ Trabajo final/Trabajo final/Datos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_final.Clases
+{
+    public class Datos
+    {
+        public struct Usuario1
+        {
+            public long username;
+            public string Nombre;
+            public int clave;
+            public string rol;
+            public DateTime fecha_creacion;
+            public bool actividad;
+
+        }
+        
+
+        public static void Usuarios()
+        {
+            const int MAX = 4;
+            Usuario1[] U1 = new Usuario1[MAX];
+
+         //Datos del Primer usuario
+            U1[0].username = 05500014500;
+            U1[0].Nombre = "Miguel Aurelino";
+            U1[0].clave = 1234;
+            U1[0].rol = "Supervisor";
+            U1[0].fecha_creacion = new DateTime(2020, 2, 15);
+            U1[0].actividad = true;
+
+            //Datos del segundo usuario
+            U1[1].username = 40225053392;
+            U1[1].Nombre = "Aderly Ramirez";
+            U1[1].clave = 1234;
+            U1[1].rol = "Administrador";
+            U1[1].fecha_creacion = new DateTime(2015, 3, 2);
+            U1[1].actividad = true;
+
+            //Datos del tercer usuario
+            U1[2].username = 00114219033;
+            U1[2].Nombre = "Arismendy Mañon ";
+            U1[2].clave = 324242;
+            U1[2].rol = "Vendedor";
+            U1[2].fecha_creacion = new DateTime(2010, 10, 10);
+            U1[2].actividad = true;
+
+            //Datos del cuarto usuario
+            U1[3].username = 40224382370;
+            U1[3].Nombre = "Gladeline Rapozo";
+            U1[3].clave = 5453;
+            U1[3].rol = "Vendedor";
+            U1[3].fecha_creacion = new DateTime(2019, 8, 25);
+            U1[3].actividad = false;
+
+            Program.Login(U1);
+        }
+        
+       
+       
+
+    }
+}
---
+++ Trabajo final/Trabajo final/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Trabajo_final.Clases;
+
+namespace Trabajo_final
+{
+    class Program:Datos
+    {
+       
+         public static void Login(Usuario1 [] U1)
+        {
+
+            bool acceso = false;
+            long username = 0;
+            int contrasena = 0;
+            
+            
+
+            do
+            {
+                int c = 0;
+                Console.Clear();
+                Console.WriteLine("DATO IMPORTANTE: EL NOMBRE DE USUARIO ES EL NUMERO DE CEDULA SIN GUIONES ");
+            Vuelve:
+                try
+                {
+                    Console.Write("Ingrese su nombre de usuario: ");
+                    username = long.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("ERROR ");
+                    Console.WriteLine("DATO ERRONEO ");
+                    Console.WriteLine("Pulse Enter para ingresar el dato nuevamente...");
+                    goto Vuelve;
+                }
+
+                Console.WriteLine();
+            Vuelve1:
+                try
+                {
+                    Console.Write("Ingrese su contraseña: ");
+                    contrasena = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("ERROR ");
+                    Console.WriteLine("DATO ERRONEO ");
+                    Console.WriteLine("Pulse Enter para ingresar el dato nuevamente...");
+                    goto Vuelve1;
+                }
+
+
+                for (int i = 0; i < 4; i++)
+                {
+
+
+                    if (username == U1[i].username)
+                    {
+                        if (contrasena == U1[i].clave)
+                        {
+                            if (U1[i].actividad==true)
+                            {
+                                acceso = true;
+                                Console.WriteLine("ACCESO CONCEDIDO...");
+                                Console.WriteLine("El nombre del usuario es: " + U1[i].Nombre);
+                                Console.WriteLine("Su Usuario es: " + U1[i].username);
+                                Console.WriteLine("Su rol es: " + U1[i].rol);
+                                Console.WriteLine("La feca de creacion del usario es: " + U1[i].fecha_creacion.ToShortDateString());
+                                Console.WriteLine("Su estado de actividad es: " + U1[i].actividad);
+                                Vuelta(U1);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El usuario {0} está inactivo  ",U1[i].username);
+                                Console.WriteLine("Valide otro Usuario...");
+                                Console.WriteLine("PULSE ENTER PARA CONTINUAR");
+                                Console.ReadLine();
+                            }
+                          
+                        }
+                        else
+                        {
+                            c++;
+                        }
+                    }
+                    else
+                    {
+                        c++;
+                    }
+                }
+                if (c==4)
+                {
+                    TimeSpan dormida = new TimeSpan(0, 0, 2);
+                    Console.WriteLine("Dato de Usuario Incorrecto");
+                    Console.WriteLine("Espere 2 segundo para volver a intentarlo ");
+                    Thread.Sleep(dormida);
+                    
+                }
+               
+            } while (acceso == false );
+
+        }
+        public static void Vuelta(Usuario1[] U1)
+        {
+            string vuelta = "";
+            Console.WriteLine("¿Desea validar otro usuario? Digite (si) o (no)");
+            vuelta = Console.ReadLine();
+
+            switch(vuelta)
+            {
+                case "si":
+                    Login(U1);
+                    break;
+                case "Si":
+                    Login(U1);
+                    break;
+                case "SI":
+                    Login(U1);
+                    break;
+                case "no":
+                    Console.WriteLine("CERRANDO EL PROGRAMA...");
+                    Console.WriteLine("Pulse Enter...");
+                    break;
+                case "No":
+                    Console.WriteLine("CERRANDO EL PROGRAMA...");
+                    Console.WriteLine("Pulse Enter...");
+                    break;
+                case "NO":
+                    Console.WriteLine("CERRANDO EL PROGRAMA...");
+                    Console.WriteLine("Pulse Enter...");
+                    break;
+                default:
+                    Console.WriteLine("Dato Erroneo ingresado");
+                    Vuelta(U1);
+                    break;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            Datos.Usuarios();
+
+            Console.ReadKey();
+
+        }
+    }
+}
